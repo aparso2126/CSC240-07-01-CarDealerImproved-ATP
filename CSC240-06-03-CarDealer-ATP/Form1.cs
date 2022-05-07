5fb58e6705c3febcc5d5c6d100133afa1938b664
Austin <aparso2126@my.sullivan.edu>
2022-05-07 16:24:13 -0400

    Update Author Comments

--- CSC240-06-03-CarDealer-ATP/Form1.cs
+++ CSC240-06-03-CarDealer-ATP/Form1.cs
@@ -1,3 +1,11 @@
+/**************************************
+ * Name:        Austin Parsons
+ * Date:        2022-05-07
+ * Description: A program that displays information about
+ * different car models by using radio buttons
+ * Assistance:
+ *************************************/
+
 namespace CSC240_06_03_CarDealer_ATP
 {
     public partial class Form1 : Form
