6cecd4b2e99e6ff5de4a67949dab71e365edad0c
Austin <aparso2126@my.sullivan.edu>
2022-05-15 02:04:38 -0400

    Created DropBox, PictureBox. Coded Image Downloader

--- CSC240-06-03-CarDealer-ATP/CarDealerForm.Designer.cs
+++ CSC240-06-03-CarDealer-ATP/CarDealerForm.Designer.cs
@@ -30,13 +30,16 @@
         {
             this.UxJeepModelsLabel = new System.Windows.Forms.Label();
             this.jeepModelsComboBox = new System.Windows.Forms.ComboBox();
+            this.carPictureBox = new System.Windows.Forms.PictureBox();
+            this.viewSpecsButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.carPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // UxJeepModelsLabel
             // 
             this.UxJeepModelsLabel.AutoSize = true;
             this.UxJeepModelsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.UxJeepModelsLabel.Location = new System.Drawing.Point(22, 24);
+            this.UxJeepModelsLabel.Location = new System.Drawing.Point(123, 25);
             this.UxJeepModelsLabel.Name = "UxJeepModelsLabel";
             this.UxJeepModelsLabel.Size = new System.Drawing.Size(171, 29);
             this.UxJeepModelsLabel.TabIndex = 1;
@@ -45,27 +48,48 @@
             // jeepModelsComboBox
             // 
             this.jeepModelsComboBox.FormattingEnabled = true;
-            this.jeepModelsComboBox.Items.AddRange(new object[] {
-            "test1",
-            "test2"});
-            this.jeepModelsComboBox.Location = new System.Drawing.Point(22, 73);
+            this.jeepModelsComboBox.Location = new System.Drawing.Point(123, 74);
             this.jeepModelsComboBox.MaxDropDownItems = 10;
             this.jeepModelsComboBox.Name = "jeepModelsComboBox";
-            this.jeepModelsComboBox.Size = new System.Drawing.Size(311, 28);
+            this.jeepModelsComboBox.Size = new System.Drawing.Size(220, 28);
             this.jeepModelsComboBox.TabIndex = 2;
             this.jeepModelsComboBox.SelectedIndexChanged += new System.EventHandler(this.jeepModelsComboBox_SelectedIndexChanged);
+            // 
+            // carPictureBox
+            // 
+            this.carPictureBox.ImageLocation = "https://media.motorfuse.com/img.cfm/type/2/img/0E16CF4B4F698D85C6FDAAC6AC2D0A0CC0" +
+    "EB75A63ABD5D74";
+            this.carPictureBox.Location = new System.Drawing.Point(22, 192);
+            this.carPictureBox.Name = "carPictureBox";
+            this.carPictureBox.Size = new System.Drawing.Size(427, 320);
+            this.carPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.carPictureBox.TabIndex = 3;
+            this.carPictureBox.TabStop = false;
+            // 
+            // viewSpecsButton
+            // 
+            this.viewSpecsButton.Location = new System.Drawing.Point(123, 108);
+            this.viewSpecsButton.Name = "viewSpecsButton";
+            this.viewSpecsButton.Size = new System.Drawing.Size(220, 33);
+            this.viewSpecsButton.TabIndex = 4;
+            this.viewSpecsButton.Text = "View Specs";
+            this.viewSpecsButton.UseVisualStyleBackColor = true;
             // 
             // CarDealerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(371, 229);
+            this.ClientSize = new System.Drawing.Size(467, 524);
+            this.Controls.Add(this.viewSpecsButton);
+            this.Controls.Add(this.carPictureBox);
             this.Controls.Add(this.jeepModelsComboBox);
             this.Controls.Add(this.UxJeepModelsLabel);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "CarDealerForm";
             this.Text = "Car Dealer";
+            this.Load += new System.EventHandler(this.CarDealerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.carPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,5 +99,7 @@
 
         private Label UxJeepModelsLabel;
         private ComboBox jeepModelsComboBox;
+        private PictureBox carPictureBox;
+        private Button viewSpecsButton;
     }
 }
--- CSC240-06-03-CarDealer-ATP/CarDealerForm.cs
+++ CSC240-06-03-CarDealer-ATP/CarDealerForm.cs
@@ -18,7 +18,46 @@ namespace CSC240_07_01_CarDealerImproved_ATP
 
         private void jeepModelsComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            jeepModelsComboBox.Items.Add("Test9");
+            string currentIndex = jeepModelsComboBox.SelectedIndex.ToString();
+            var timeoutInMilliseconds = 5000;
+            var uri = new Uri("https://www.globalautomall.com/vehicles.cfm/make/jeep/");
+            var doc = Supremes.Dcsoup.Parse(uri, timeoutInMilliseconds);
+            string modelURL = "https://www.globalautomall.com" + doc.GetElementsByClass("col-sm-9").ElementAt(Int32.Parse(currentIndex)).GetElementsByTag("a").ElementAt(0).Attr("href");
+
+            if (currentIndex == "4") {
+                carPictureBox.ImageLocation = "https://media.motorfuse.com/img.cfm/type/2/img/0E16CC4049698D85C6FDAAC6AC2D0A0CC0EB71D23ABD5D74";
+                return;
+            }
+
+            carPictureBox.ImageLocation = getCarImage(modelURL);
+        }
+
+        private void CarDealerForm_Load(object sender, EventArgs e)
+        {
+            var timeoutInMilliseconds = 5000;
+            var uri = new Uri("https://www.globalautomall.com/vehicles.cfm/make/jeep/");
+            var doc = Supremes.Dcsoup.Parse(uri, timeoutInMilliseconds);
+
+            // <span itemprop="ratingValue">86</span>
+            var modelCount = doc.GetElementsByClass("col-sm-9").Count;
+
+            for (int i = 0; i < modelCount; i += 1)
+            {
+                string modelStr = doc.GetElementsByClass("col-sm-9").ElementAt(i).GetElementsByClass("notranslate").ElementAt(0).Text.Substring(5);
+                jeepModelsComboBox.Items.Add(modelStr);
+            }
+            jeepModelsComboBox.SelectedIndex = 0;
+        }
+
+        private static string getCarImage(string URL)
+        {
+            var timeoutInMilliseconds = 5000;
+            var uri = new Uri(URL);
+            var doc = Supremes.Dcsoup.Parse(uri, timeoutInMilliseconds);
+
+            int imageIndex = doc.GetElementsByClass("row").ElementAt(1).GetElementsByClass("col-sm-3").Count()-1;
+            string imageSrc = doc.GetElementsByClass("row").ElementAt(1).GetElementsByClass("col-sm-3").ElementAt(imageIndex).GetElementsByTag("img").ElementAt(0).Attr("data-src");
+            return imageSrc;
         }
     }
 }
